975bd9d1e4b699973882d15e02a1a15db2875232
Dan Foad <danielfoad@gmail.com>
2015-05-09 19:33:07 +0100

    Added functionality for fill type hold buttons

--- jquery.holdbutton.js
+++ jquery.holdbutton.js
@@ -16,9 +16,44 @@
         return this.each(function() {
             var $this = $(this);
             var progress;
+            var timeoutId;
+            var animation;
+            var overlay;
 
             switch (settings.type) {
                 case "fill":
+                    var width = $this.width();
+                    var height = $this.height();
+                    var left = $this.offset().left;
+                    var top = $this.offset().top;
+                    var defaultCSS = {
+                        "position": "absolute",
+                        "left": left,
+                        "top": top,
+                        "width": 0,
+                        "height": height,
+                        "z-index": 9001,
+                        "background-color": holdColour
+                    };
+
+                    overlay = $("<div></div>").css(defaultCSS);
+
+                    $("body").append(overlay);
+
+                    $this.mousedown(function() {
+                        timeoutId = setTimeout(function() {
+                            overlay.css("background-color", settings.successColour);
+                            callback();
+                        }, settings.duration);
+                        animation = overlay.animate({
+
+                        }, settings.duration);
+                    }).bind("mouseup mouseleave", function() {
+                        clearTimeout(timeoutId);
+                        animation.stop();
+                        overlay.css(defaultCSS);
+                    });
+
                     break;
                 case "bar":
                     break;
